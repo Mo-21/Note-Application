f04d4279084ab1a392b557659e0ff34be5565fbd
Mohammad Malaeb <m.malaeb21@gmail.com>
2023-12-06 11:49:35 +0300

    Refactor NewNote component and add title input

--- src/components/NewNote.tsx
+++ src/components/NewNote.tsx
@@ -2,11 +2,14 @@ import { EditorContent, useEditor } from "@tiptap/react";
 import "../styles/NewNote.css";
 import StarterKit from "@tiptap/starter-kit";
 import MenuBar from "./MenuBar";
+import { useLocalStorage } from "./useLocalStorage";
+import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
 function NewNote() {
   const editor = useEditor({
     extensions: [StarterKit],
-    content: "<p>Write Something</p> <h1>Hello World! 🌎️</h1>",
+    content: "<h1>Hello World! 🌎️</h1> <p>Write Something</p>",
     editorProps: {
       attributes: {
         class: "new-note",
@@ -14,13 +17,44 @@ function NewNote() {
     },
   });
 
+  const inputRef = useRef<HTMLInputElement>(null);
+  const { setItem } = useLocalStorage("New Note");
+
+  const navigate = useNavigate();
+
   return (
     <>
       <MenuBar editor={editor} />
+      <input
+        className="title-input"
+        maxLength={200}
+        type="text"
+        placeholder="Title"
+        ref={inputRef}
+        required
+      />
       <EditorContent className="editor-content" editor={editor} />
       <div className="action-buttons">
-        <button className="create-btn">Create</button>
-        <button className="cancel-btn">Cancel</button>
+        <button
+          onClick={() =>
+            setItem({
+              title: inputRef.current?.value || "New Note",
+              content: editor?.getHTML(),
+              createdAt: new Date().toLocaleString(),
+            })
+          }
+          className="create-btn"
+        >
+          Create
+        </button>
+        <button
+          onClick={() => {
+            navigate("/");
+          }}
+          className="cancel-btn"
+        >
+          Cancel
+        </button>
       </div>
     </>
   );
